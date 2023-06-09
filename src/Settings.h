6cd9d733e90a499ac8a7d9506bdaf55c7ffdc9dc
Erinell <ceddebois@gmail.com>
2023-06-09 15:03:34 +0200

    Ajout de paramètres

--- src/Settings.h
+++ src/Settings.h
@@ -20,3 +20,17 @@
  Si désactivé, la broche RESET de la carte doit être connectée à la broche reset définie dans Pins.h
 */
 #define SOFT_RESET false
+
+// Valeurs de score associées aux broches SCORE (voir Pins.h)
+#define NB_SCORE 4
+#define SCORE_VALUE {10, 100, 1000, 10000}
+
+// Valeurs des bonus associées aux broches BONUS (voir Pins.h)
+// Les valeurs sont multipliées par 2 si le bnous est actif
+#define NB_BONUS 4
+#define BONUS_VALUE {150, 250, 750, 1500}
+
+// Valeurs des cibles associées aux broches TARGETS (voir Pins.h)
+// Les cibles remontent lors d'une nouvelle bille
+#define NB_TARGET 4
+#define TARGET_VALUE {1000, 1000, 1000, 1000}
