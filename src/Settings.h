0d144c5419985aaf3e0891a680fd49ee7e3c8bf5
Erinell <ceddebois@gmail.com>
2023-06-12 15:44:17 +0200

    simplification

--- src/Settings.h
+++ src/Settings.h
@@ -22,15 +22,16 @@
 #define SOFT_RESET false
 
 // Valeurs de score associées aux broches SCORE (voir Pins.h)
-#define NB_SCORE 4
 #define SCORE_VALUE {10, 100, 1000, 10000}
 
-// Valeurs des bonus associées aux broches BONUS (voir Pins.h)
-// Les valeurs sont multipliées par 2 si le bnous est actif
-#define NB_BONUS 4
+/*
+ Valeurs des bonus associées aux broches BONUS (voir Pins.h)
+ Les valeurs sont multipliées par 2 si le bonus est actif
+*/
 #define BONUS_VALUE {150, 250, 750, 1500}
 
-// Valeurs des cibles associées aux broches TARGETS (voir Pins.h)
-// Les cibles remontent lors d'une nouvelle bille
-#define NB_TARGET 4
-#define TARGET_VALUE {1000, 1000, 1000, 1000}
+/*
+ Valeurs des cibles associées aux broches TARGETS (voir Pins.h)
+ Les cibles remontent lors d'une nouvelle bille
+*/
+#define TARGET_VALUE {1000, 1000, 1000, 1000, 1000}
